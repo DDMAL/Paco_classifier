cd4317f402a8d8439afb078802ed6afdd87df1a3
Gabriel Vigliensoni <gabriel@vigliensoni.com>
2017-06-07 13:07:23 -0400

    Add: Template class for the classifier

---
+++ calvo_classifier.py
@@ -0,0 +1,15 @@
+#-----------------------------------------------------------------------------
+# Program Name:         calvo_classifier.py
+# Program Description:  Rodan wrapper for Calvo's classifier
+#-----------------------------------------------------------------------------
+
+from rodan.jobs.base import RodanTask
+
+
+class CalvoClassifier(RodanTask)
+    """Wrap Calvo classifier in Rodan."""
+
+    def run_my_task(self, inputs, settings, outputs):
+        """Run the process."""
+
+        return True
