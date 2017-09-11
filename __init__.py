b4e41477370e6ad7fe772bb6b5044ff1049d772d
Jorge Calvo <jcalvo@dlsi.ua.es>
2017-09-11 17:03:27 +0200

    Disable pixel-by-pixel (slow) Calvo's classifier

--- __init__.py
+++ __init__.py
@@ -6,6 +6,6 @@ logger = logging.getLogger('rodan')
 
 from rodan.jobs import module_loader
 
-module_loader('rodan.jobs.Calvo-classifier.calvo_classifier')
+#module_loader('rodan.jobs.Calvo-classifier.calvo_classifier')
 module_loader('rodan.jobs.Calvo-classifier.fast_calvo_classifier')
 
