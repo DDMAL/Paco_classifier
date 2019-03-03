fb11fa58ea1e4de1790e9872eb475429b104e506
Jorge Calvo Zaragoza <jcalvo@Air-de-Jorge.home>
2019-03-03 10:58:54 +0100

    Correcting some minor errors in the code (name of variables mostly)

--- training_engine_sae.py
+++ training_engine_sae.py
@@ -66,13 +66,17 @@ def getTrain(input_image, gt, patch_height, patch_width):
     X_train = []
     Y_train = {}
 
+    # Initialize GT lists
+    for key in gt:
+        Y_train[key] = []
+
     hstride = patch_height // 2
     wstride = patch_width // 2
 
     # TODO Take into account margins
     for h in range(0, input_image.shape[0] - patch_height, hstride):
         for w in range(0, input_image.shape[1] - patch_width, wstride):
-            x_sample = input_image[h:h + hspan, w:w + wspan]
+            x_sample = input_image[h:h + patch_height, w:w + patch_width]
 
             # Pre-process TODO: Check all the pipelines to do the same
             x_sample = (255. - x_sample) / 255.
@@ -80,17 +84,18 @@ def getTrain(input_image, gt, patch_height, patch_width):
             X_train.append(x_sample)
 
             for label in gt:
-                Y_train[label].append(gt[label][h:h + hspan, w:w + wspan])
+                Y_train[label].append(gt[label][h:h + patch_height, w:w + patch_width])
 
     return X_train, Y_train
 
 
 def train_msae(input_image, gt, height, width, output_path, epochs):
-    # -------------------------------------------------------------------------------------------------------------------
 
     # Crop the image and create ground_truth
     [X_train, Y_train] = getTrain(input_image, gt,
                                   height, width)
+
+    X_train = np.asarray(X_train)
 
     print 'Training created with ' + str(len(X_train)) + ' samples.'
     for label in Y_train:
@@ -109,8 +114,12 @@ def train_msae(input_image, gt, height, width, output_path, epochs):
                 EarlyStopping(monitor='val_acc', patience=3, verbose=0, mode='max')
                 ]
 
+        Y_train_label = np.expand_dims(np.asarray(Y_train[label]),axis=-1)
+
+        print(X_train.shape)
+        print(Y_train_label.shape)
         # Training stage
-        model.fit(X_train, Y_train[label],
+        model.fit(X_train, Y_train_label,
                   verbose=2,
                   batch_size=BATCH_SIZE,
                   validation_split=VALIDATION_SPLIT,
@@ -119,34 +128,9 @@ def train_msae(input_image, gt, height, width, output_path, epochs):
 
     return 0
 
-# Debuggin code
+
+# Debugging code
 if __name__ == "__main__":
-    input_path = '/Users/jcalvo/Desktop/McGill/dama-corpora/Corpora/Layers/Salzinnes/CF-009/'
-
-    # Read original image
-    input_image = cv2.imread(input_path + '/CF-009.jpg', True)  # 3-channel
-
-    # Open layers as expected
-    background = cv2.imread(input_path + '/CF-009_Decoration.jpg',cv2.IMREAD_UNCHANGED)  # 4-channel
-    notes = cv2.imread(input_path + '/CF-009_Notes.jpg',cv2.IMREAD_UNCHANGED)  # 4-channel
-    lines = cv2.imread(input_path + '/CF-009_Staff.jpg',cv2.IMREAD_UNCHANGED)  # 4-channel
-    text = cv2.imread(input_path + '/CF-009_Text.jpg',cv2.IMREAD_UNCHANGED)  # 4-channel
-
-    gt = {}
-    gt['symbols'] = (notes[:, :, 3] == 255)
-    gt['staff'] = (lines[:, :, 3] == 255)
-    gt['text'] = (text[:, :, 3] == 255)
-    gt['background'] = (background[:, :,3] == 255)  # background is already restricted to the selected regions (based on Pixel.js' behaviour)
-
-    # Indicate paths
-    output = {}
-    path = '/Users/jcalvo/Desktop/McGill/Models/'
-    output['symbols'] = path + '_symbols.hdf5'
-    output['staff'] = path + '_staff.hdf5'
-    output['text'] = path + '_text.hdf5'
-    output['background'] = path +'bg.hdf5'
-
-    # Call train_msae
-    train_msae(input,gt,256,256,output,epochs=3)
+    print('Must be run from Rodan')
 
 
