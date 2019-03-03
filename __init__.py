e267f361be3fce47f4fdb7c65588176d0852d761
Jorge Calvo Zaragoza <jcalvo@Air-de-Jorge.home>
2019-03-03 15:26:28 +0100

    Modifying module initialization to include Fast CM trainer

--- __init__.py
+++ __init__.py
@@ -8,5 +8,6 @@ from rodan.jobs import module_loader
 
 module_loader('rodan.jobs.Calvo-classifier.calvo_classifier')
 module_loader('rodan.jobs.Calvo-classifier.calvo_trainer')
-#module_loader('rodan.jobs.Calvo-classifier.fast_calvo_classifier')
+module_loader('rodan.jobs.Calvo-classifier.fast_calvo_classifier')
+module_loader('rodan.jobs.Calvo-classifier.fast_calvo_trainer')
 
