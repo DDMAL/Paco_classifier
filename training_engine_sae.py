6e840344b1ca307c1b54e2150d3dd87bce81ebed
Gabriel Vigliensoni <gabriel@vigliensoni.com>
2019-09-25 19:20:41 -0400

    Fix: Monitor name.
    For some reason, `val_acc` was not being  picked as monitor. Therefore,
    Keras was complaining with "Can save best model only with
    `val_acc` available, skipping."
    
    Using `val_accuracy` fixes this problem.

--- training_engine_sae.py
+++ training_engine_sae.py
@@ -113,8 +113,8 @@ def train_msae(input_image, gt, height, width, output_path, epochs):
         model.summary()
 
         callbacks_list = [
-                ModelCheckpoint(output_path[label], save_best_only=True, monitor='val_acc', verbose=1, mode='max'),
-                EarlyStopping(monitor='val_acc', patience=3, verbose=0, mode='max')
+                ModelCheckpoint(output_path[label], save_best_only=True, monitor='val_accuracy', verbose=1, mode='max'),
+                EarlyStopping(monitor='val_accuracy', patience=3, verbose=0, mode='max')
                 ]
 
         Y_train_label = np.expand_dims(np.asarray(Y_train[label]),axis=-1)
