94d97a0fafed22c63b59a10a980bf929acb2e9e7
deepio <alex.daigle2@gmail.com>
2019-10-02 10:14:13 -0400

    Update fast_calvo_classifier.py

--- fast_calvo_classifier.py
+++ fast_calvo_classifier.py
@@ -44,23 +44,20 @@ class FastCalvoClassifier(RodanTask):
         },
         'job_queue': 'Python3'
     }
-    
+
     input_port_types = (
         {'name': 'Image', 'minimum': 1, 'maximum': 100, 'resource_types': lambda mime: mime.startswith('image/')},
-        {'name': 'Background model',    'minimum': 1, 'maximum': 1, 'resource_types': ['keras/model+hdf5'] },
-        {'name': 'Symbol model',        'minimum': 1, 'maximum': 1, 'resource_types': ['keras/model+hdf5'] },
-        {'name': 'Staff-line model',    'minimum': 1, 'maximum': 1, 'resource_types': ['keras/model+hdf5'] },
-        {'name': 'Text model',          'minimum': 1, 'maximum': 1, 'resource_types': ['keras/model+hdf5'] },
+        {'name': 'Background model', 'minimum': 1, 'maximum': 1, 'resource_types': ['keras/model+hdf5']},
+        {'name': 'Symbol model', 'minimum': 1, 'maximum': 1, 'resource_types': ['keras/model+hdf5']},
+        {'name': 'Staff-line model', 'minimum': 1, 'maximum': 1, 'resource_types': ['keras/model+hdf5']},
+        {'name': 'Text model', 'minimum': 1, 'maximum': 1, 'resource_types': ['keras/model+hdf5']},
     )
-    output_port_types = (    
+    output_port_types = (
         {'name': 'Background', 'minimum': 0, 'maximum': 100, 'resource_types': ['image/rgba+png']},
         {'name': 'Music symbol', 'minimum': 0, 'maximum': 100, 'resource_types': ['image/rgba+png']},
         {'name': 'Staff lines', 'minimum': 0, 'maximum': 100, 'resource_types': ['image/rgba+png']},
-        {'name': 'Text', 'minimum': 0, 'maximum': 100, 'resource_types': ['image/rgba+png']}        
+        {'name': 'Text', 'minimum': 0, 'maximum': 100, 'resource_types': ['image/rgba+png']}
     )
-    
-    
-    
 
     """
     Entry point
@@ -76,46 +73,46 @@ class FastCalvoClassifier(RodanTask):
         text_model = inputs['Text model'][0]['resource_path']
 
         model_paths = [background_model, symbol_model, staff_model, text_model]
-        
+
         # Settings        
-        height =  settings['Height']
-        width =  settings['Width']
-        threshold = settings['Threshold']         
-        
+        height = settings['Height']
+        width = settings['Width']
+        threshold = settings['Threshold']
+
         for idx in range(len(inputs['Image'])):
             image_filepath = inputs['Image'][idx]['resource_path']
             # Process
-            image = cv2.imread(image_filepath,True)            
-            
-            analyses = recognition.process_image_msae(image,model_paths,height,width, mode = mode)        
-            
-            for id_label in range(len(model_paths)):
+            image = cv2.imread(image_filepath,True)
+
+            analyses = recognition.process_image_msae(image, model_paths, height, width, mode = mode)
+
+            for id_label, model in enumerate(model_paths):
                 if mode == 'masks':
-                    mask = ((analyses[id_label] > (threshold/100.0))*255).astype('uint8')
+                    mask = ((analyses[id_label] > (threshold / 100.0)) * 255).astype('uint8')
                 elif mode == 'logical':
                     label_range = np.array(id_label, dtype=np.uint8)
                     mask = cv2.inRange(analyses, label_range, label_range)
-      
-                original_masked = cv2.bitwise_and(image,image,mask = mask)      
+
+                original_masked = cv2.bitwise_and(image,image,mask = mask)
                 original_masked[mask == 0] = (255, 255, 255)
-            
+
                 # Alpha = 0 when background
-                alpha_channel = np.ones(mask.shape, dtype=mask.dtype)*255
-                alpha_channel[mask == 0] = 0            
+                alpha_channel = np.ones(mask.shape, dtype=mask.dtype) * 255
+                alpha_channel[mask == 0] = 0
                 b_channel, g_channel, r_channel = cv2.split(original_masked)
                 original_masked_alpha = cv2.merge((b_channel, g_channel, r_channel, alpha_channel))
-    
+
                 if id_label == 0:
-                    port = 'Background'            
+                    port = 'Background'
                 elif id_label == 1:
                     port = 'Music symbol'
                 elif id_label == 2:
                     port = 'Staff lines'
                 elif id_label == 3:
-                    port = 'Text'                
-                    
+                    port = 'Text'
+
                 if port in outputs:
-                    cv2.imwrite(outputs[port][idx]['resource_path']+'.png',original_masked_alpha)   
+                    cv2.imwrite(outputs[port][idx]['resource_path']+'.png',original_masked_alpha)
                     os.rename(outputs[port][idx]['resource_path']+'.png',outputs[port][idx]['resource_path'])
-       
+
         return True
