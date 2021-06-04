8eb6fd10e4b49396162ba53e0971f1cdd3caa227
Kemal Kongar <47402993+kemalkongar@users.noreply.github.com>
2021-06-04 09:28:26 -0400

    Update __init__.py
    Changed folder names to use underscores rather than hyphens.

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ logger = logging.getLogger('rodan')
 
 from rodan.jobs import module_loader
 
-module_loader('rodan.jobs.Calvo-classifier.calvo_classifier')
-module_loader('rodan.jobs.Calvo-classifier.calvo_trainer')
-module_loader('rodan.jobs.Calvo-classifier.fast_calvo_classifier')
-module_loader('rodan.jobs.Calvo-classifier.fast_calvo_trainer')
+module_loader('rodan.jobs.Calvo_classifier.calvo_classifier')
+module_loader('rodan.jobs.Calvo_classifier.calvo_trainer')
+module_loader('rodan.jobs.Calvo_classifier.fast_calvo_classifier')
+module_loader('rodan.jobs.Calvo_classifier.fast_calvo_trainer')
