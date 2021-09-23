9c206c7686f379664a77fa6eb146a1d6953a6373
Kemal Kongar <47402993+kemalkongar@users.noreply.github.com>
2021-09-23 16:28:33 -0400

    added self.

--- fast_trainer_lib.py
+++ fast_trainer_lib.py
@@ -43,8 +43,8 @@ class CalvoTrainer:
 
     def runTrainer(self):
 
-        input_ports = len([x for x in inputs if "Layer" in x])
-        output_ports = len([x for x in outputs if "Model" in x or "Log file" in x])
+        input_ports = len([x for x in self.inputs if "Layer" in x])
+        output_ports = len([x for x in self.outputs if "Model" in x or "Log file" in x])
         if input_ports not in [output_ports, output_ports - 1]: # So it still works if Log File is added as an output. 
             raise Exception(
                 'The number of input layers "rgba PNG - Layers" does not match the number of'
@@ -56,21 +56,21 @@ class CalvoTrainer:
         output_models_path = {}
 
         for i in range(input_ports):
-            output_models_path[str(i)] = outputs["Model " + str(i)][0]["resource_path"]
+            output_models_path[str(i)] = self.outputs["Model " + str(i)][0]["resource_path"]
             # THIS IS NOT TAKING INTO ACCOUNT ANY FILE NOT NAMED MODEL IE BACKGROUND AND LOG!!!!
 
         # Call in training function
         status = training.train_msae(
-            inputs=inputs,
-            num_labels=input_ports,
-            height=patch_height,
-            width=patch_width,
-            output_path=output_models_path,
-            file_selection_mode=file_selection_mode,
-            sample_extraction_mode=sample_extraction_mode,
-            epochs=max_number_of_epochs,
-            number_samples_per_class=max_samples_per_class,
-            batch_size=batch_size,
+            inputs=self.inputs,
+            num_labels=self.input_ports,
+            height=self.patch_height,
+            width=self.patch_width,
+            output_path=self.output_models_path,
+            file_selection_mode=self.file_selection_mode,
+            sample_extraction_mode=self.sample_extraction_mode,
+            epochs=self.max_number_of_epochs,
+            number_samples_per_class=self.max_samples_per_class,
+            batch_size=self.batch_size,
         )
         print("Finishing the Fast CM trainer job.")
         for i in range(input_ports):
