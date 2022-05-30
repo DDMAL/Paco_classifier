4de63e179bc6d224c9e1560cf242dea4685d7f6f
softcat477 <softcat477@gmail.com>
2022-05-30 13:49:45 -0400

    Settings for local paco job

--- __init__.py
+++ __init__.py
@@ -6,7 +6,5 @@ logger = logging.getLogger('rodan')
 
 from rodan.jobs import module_loader
 
-module_loader('rodan.jobs.Calvo_classifier.calvo_classifier')
-module_loader('rodan.jobs.Calvo_classifier.calvo_trainer')
-module_loader('rodan.jobs.Calvo_classifier.fast_calvo_classifier')
-module_loader('rodan.jobs.Calvo_classifier.fast_calvo_trainer')
+module_loader('rodan.jobs.Paco_classifier.fast_calvo_classifier')
+module_loader('rodan.jobs.Paco_classifier.fast_calvo_trainer')
